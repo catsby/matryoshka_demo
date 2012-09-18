ce1363c8ccebc4a8dc57bde72f27b96222d7e2aa
Clint Shryock <clint@ctshryock.com>
2012-09-18 13:53:29 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RussianDoll::Application.config.secret_token = 'd3321803bc88d0706cf237074348c82e784fae89bb6fb230060805b95e76621ac5fb3608e6f37c81b68fc51b371a1f1bbdfd5a1c13288de43e220b86bc98e27a'
